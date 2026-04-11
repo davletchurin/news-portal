147060e8b8490d64fed11825a6916c141cd08523
Ainur Davletchurin <davletchurin@gmail.com>
2026-04-11 23:50:50 +0500

    feat(deps): update dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,8 @@
+import org.springframework.boot.gradle.tasks.bundling.BootJar
+
 plugins {
 	java
+	application
 	id("org.springframework.boot") version "3.5.11"
 	id("io.spring.dependency-management") version "1.1.7"
 }
@@ -14,29 +17,43 @@ java {
 	}
 }
 
+application {
+	mainClass.set("com.example.news_portal.NewsPortalApplication")
+}
+
 repositories {
 	mavenCentral()
 }
 
 dependencies {
-	implementation("org.springframework.boot:spring-boot-starter")
 	implementation("org.springframework.boot:spring-boot-starter-web")
 	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
 	implementation("org.springframework.boot:spring-boot-starter-validation")
+
 	runtimeOnly("org.postgresql:postgresql")
+	//implementation("org.liquibase:liquibase-core")
 
 	compileOnly("org.projectlombok:lombok:1.18.44")
 	annotationProcessor("org.projectlombok:lombok:1.18.44")
 	testCompileOnly("org.projectlombok:lombok:1.18.44")
 	testAnnotationProcessor("org.projectlombok:lombok:1.18.44")
 
-	implementation("org.mapstruct:mapstruct:1.5.3.Final")
-	annotationProcessor("org.mapstruct:mapstruct-processor:1.5.3.Final")
+	implementation("org.mapstruct:mapstruct:1.6.3")
+	annotationProcessor("org.mapstruct:mapstruct-processor:1.6.3")
+	annotationProcessor("org.projectlombok:lombok-mapstruct-binding:0.2.0")
 
 	implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.8.16")
 
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+}
+
+tasks.named("jar") {
+	enabled = false
+}
+
+tasks.withType<BootJar> {
+	archiveFileName.set("application.jar")
 }
 
 tasks.withType<Test> {
