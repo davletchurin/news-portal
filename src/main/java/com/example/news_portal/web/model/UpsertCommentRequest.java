43792ccec8cb7e0ea777891c352e248254e5d84b
Ainur Davletchurin <davletchurin@gmail.com>
2026-04-13 04:13:32 +0500

    feat(core): add valid annotations

--- src/main/java/com/example/news_portal/web/model/UpsertCommentRequest.java
+++ src/main/java/com/example/news_portal/web/model/UpsertCommentRequest.java
@@ -3,11 +3,13 @@ package com.example.news_portal.web.model;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 @Data
 public class UpsertCommentRequest {
     @NotBlank(message = "Comment description must be specified!")
+    @Size(min = 1, max = 500, message = "Description cannot be less than {min} and greater than {max}!")
     private String description;
     @NotNull(message = "User ID must be specified!")
     @Positive(message = "User ID must be greater than 0!")
--- src/main/java/com/example/news_portal/web/model/UpsertNewsRequest.java
+++ src/main/java/com/example/news_portal/web/model/UpsertNewsRequest.java
@@ -3,13 +3,16 @@ package com.example.news_portal.web.model;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 @Data
 public class UpsertNewsRequest {
     @NotBlank(message = "News title must be specified!")
+    @Size(min = 1, max = 100, message = "Title cannot be less than {min} and greater than {max}!")
     private String title;
     @NotBlank(message = "News description must be specified!")
+    @Size(min = 1, max = 500, message = "Description cannot be less than {min} and greater than {max}!")
     private String description;
     @NotNull(message = "Category ID must be specified!")
     @Positive(message = "Category ID must be greater than 0!")
