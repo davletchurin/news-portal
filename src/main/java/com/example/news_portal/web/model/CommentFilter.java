7be4c58267c71bb65feb6a9e9d311fa63544b889
Ainur Davletchurin <davletchurin@gmail.com>
2026-04-14 22:12:43 +0500

    feat(core): add validate annotations

--- src/main/java/com/example/news_portal/web/model/CommentFilter.java
+++ src/main/java/com/example/news_portal/web/model/CommentFilter.java
@@ -1,14 +1,23 @@
 package com.example.news_portal.web.model;
 
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.validator.constraints.Range;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class CommentFilter {
-    private Integer pageSize;
+    @NotNull(message = "Page number must be specified!")
+    @Min(value = 0, message = "Page number cannot be less than {value}")
     private Integer pageNumber;
+    @NotNull(message = "Page size must be specified!")
+    @Range(min = 1, max = 100, message = "Page size cannot be less than {min} and greater than {max}")
+    private Integer pageSize;
+    @NotNull(message = "News ID must be specified!")
+    @Min(value = 1, message = "News ID cannot be less than {value}")
     private Long newsId;
 }
--- src/main/java/com/example/news_portal/web/model/UserFilter.java
+++ src/main/java/com/example/news_portal/web/model/UserFilter.java
@@ -1,13 +1,20 @@
 package com.example.news_portal.web.model;
 
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.hibernate.validator.constraints.Range;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class UserFilter {
-    private Integer pageSize;
+    @NotNull(message = "Page number must be specified!")
+    @Min(value = 0, message = "Page number cannot be less than {value}")
     private Integer pageNumber;
+    @NotNull(message = "Page size must be specified!")
+    @Range(min = 1, max = 100, message = "Page size cannot be less than {min} and greater than {max}")
+    private Integer pageSize;
 }
