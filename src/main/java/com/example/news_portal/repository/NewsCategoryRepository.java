3a5a40b8f38454b04b2c69b4a36ab35a59cb8ab8
Ainur Davletchurin <davletchurin@gmail.com>
2026-04-16 03:54:05 +0500

    feat(core): add annotation @EntityGraph for findById() methods

--- src/main/java/com/example/news_portal/repository/NewsCategoryRepository.java
+++ src/main/java/com/example/news_portal/repository/NewsCategoryRepository.java
@@ -1,7 +1,13 @@
 package com.example.news_portal.repository;
 
 import com.example.news_portal.entity.NewsCategory;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface NewsCategoryRepository extends JpaRepository<NewsCategory, Long> {
+    @Override
+    @EntityGraph(attributePaths = {"news"})
+    Optional<NewsCategory> findById(Long aLong);
 }
--- src/main/java/com/example/news_portal/repository/UserRepository.java
+++ src/main/java/com/example/news_portal/repository/UserRepository.java
@@ -1,7 +1,13 @@
 package com.example.news_portal.repository;
 
 import com.example.news_portal.entity.User;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, Long> {
+    @Override
+    @EntityGraph(attributePaths = {"news", "comments"})
+    Optional<User> findById(Long aLong);
 }
